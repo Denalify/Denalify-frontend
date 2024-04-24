3288d22cbb1ed8ccca819d5dbdbed5a4f4b5b6dd
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-04-25 01:21:38 +0200

    error issuse

--- error.vue
+++ error.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="relative flex justify-center items-center bg-background w-screen h-screen overflow-hidden">
-		<div>
+		<div v-if="nerror">
+			<div class="flex flex-col justify-center items-center max-w-xl relative">
+				<p class="z-0 absolute bottom-1/2 translate-y-1/3 text-[18rem] font-semibold tracking-widest text-transparent text-8xl bg-clip-text bg-gradient-to-b from-darkest/30 to-transparent">{{ nerror.statusCode }}</p>
+				<p class="mt-24 text-darkest/90 z-50">{{ nerror.message }}</p>
+				<button @click="handleClearError" class="z-[50] bg-second py-2 px-4 rounded-xl text-white mt-4 duration-300 hover:bg-second/80 hover:shadow-second/50 hover:shadow-xl">Go back</button>
+			</div>
+		</div>
+		<div v-else>
 			<NuxtErrorBoundary>
 				<div class="flex flex-col justify-center items-center max-w-xl relative">
 					<p class="z-0 absolute bottom-1/2 translate-y-1/3 text-[18rem] font-semibold tracking-widest text-transparent text-8xl bg-clip-text bg-gradient-to-b from-darkest/30 to-transparent">{{ error.statusCode }}</p>
@@ -13,12 +20,11 @@
 </template>
 <script setup lang="ts">
 const route = useRoute()
-console.log(route.query)
-let error = null
-if (route.query) {
-	error = {message: route.query.error_description, statusCode: 401}
-} else {
-	error = defineProps(['error'])
+defineProps(['error'])
+
+let nerror:any;
+if (route.query.error_description) {
+	nerror = {message: route.query.error_description, statusCode: 401}
 }
 
 
