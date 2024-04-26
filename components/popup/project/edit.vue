ca7885789595f88db19e90ed1347812f521eae22
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-04-26 09:10:09 +0200

    Update edit.vue

--- components/popup/project/edit.vue
+++ components/popup/project/edit.vue
@@ -26,7 +26,7 @@
 				</div>
 				<div class="mt-4 text-white">
 
-					<button @click="editproject"  class="bg-third hover:bg-third/60 duration-100 px-4 py-2 rounded-xl">Create</button>
+					<button @click="editproject"  class="bg-third hover:bg-third/60 duration-100 px-4 py-2 rounded-xl">Save</button>
 				</div>
 			</div>
 			
